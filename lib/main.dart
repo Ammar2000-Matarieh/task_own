b9ec6ef6b2d466d82b167f42043093eb76995508
AmmarMatarieh2000 <ammarmatarieh2@gmail.com>
2025-07-23 12:36:28 +0300

    update

--- lib/main.dart
+++ lib/main.dart
@@ -4,3 +4,12 @@ import 'package:task_one_2025/app.dart';
 void main() {
   runApp(const MyApp());
 }
+
+
+// project => a 3 feture 
+// project => b 5 feture 
+// project => c 2 ftecur 
+
+// path => 1 to 2 to 3 
+// path 2 
+// 
